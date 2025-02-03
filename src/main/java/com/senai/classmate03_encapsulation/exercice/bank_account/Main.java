367d5f2abf233a68517ef935866dc0b0ae0246ec
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2025-02-03 03:40:44 -0300

    i need review this code

---
+++ src/main/java/com/senai/classmate03_encapsulation/exercice/bank_account/Main.java
@@ -0,0 +1,21 @@
+package com.senai.classmate03_encapsulation.exercice.bank_account;
+
+public class Main {
+    public static void main(String[] args) {
+        AccountUser accountUser = new AccountUser("Rafael",100.00);
+
+        System.out.println(accountUser.getBalance());
+
+        accountUser.deposit(200.00);
+        System.out.println(accountUser.getBalance());
+
+        accountUser.withdraw(1500.00);
+        System.out.println(accountUser.getBalance());
+        System.out.println("-------------");
+        AccountUser accountUser1 = new AccountUser("Pedro", -1.00);
+        System.out.println(accountUser1.getBalance());
+
+        accountUser1.deposit(10);
+        System.out.println(accountUser1.getBalance());
+    }
+}
