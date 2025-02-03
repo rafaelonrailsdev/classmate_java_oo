1826b6dc09f518bcac5ad807748e4c56601abe38
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2025-02-03 01:52:02 -0300

    oo

---
+++ src/main/java/com/senai/aula01_classe_atributo_metodos/objetos/guns/Guns.java
@@ -0,0 +1,7 @@
+package com.senai.aula01_classe_atributo_metodos.objetos.guns;
+
+public class Guns {
+
+    public Guns(String name, String rifleDeAssalto) {
+    }
+}
--- src/main/java/com/senai/aula01_classe_atributo_metodos/objetos/guns/Main.java
+++ src/main/java/com/senai/aula01_classe_atributo_metodos/objetos/guns/Main.java
@@ -1,22 +1,16 @@
 package com.senai.aula01_classe_atributo_metodos.objetos.guns;
 
-import com.senai.aula01_classe_atributo_metodos.guns.Guns;
+
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-    Guns ak47 = new Guns("AK-47",
-            "Rifle de Assalto",
-            "7.62x39mm",
-            30,
-            400.0f,
-            4.3f,
-            "plástico, madeira e ferro",
-            "Kalashnikov"
+        Guns ak47 = new Guns("AK-47",
+                "Rifle de Assalto"
 
-    );
+        );
 
         Guns gun[]= new Guns[3];
         ArrayList gunsList = new ArrayList<>();
