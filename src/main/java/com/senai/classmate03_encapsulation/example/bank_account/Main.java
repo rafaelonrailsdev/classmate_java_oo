15107fa84b749f9757f43db87b1c0d1b20e7ef91
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2025-02-02 23:47:02 -0300

    encapsulation classmate

---
+++ src/main/java/com/senai/classmate03_encapsulation/example/bank_account/Main.java
@@ -0,0 +1,9 @@
+package com.senai.classmate03_encapsulation.example.bank_account;
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+        User user = new User();
+
+    }
+}
---
+++ src/main/java/com/senai/classmate03_encapsulation/example/bank_account/User.java
@@ -0,0 +1,40 @@
+package com.senai.classmate03_encapsulation.example.bank_account;
+
+public class User {
+    private String name;
+    private String indetifier;
+    private String birth;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIndetifier() {
+        return indetifier;
+    }
+
+    public void setIndetifier(String indetifier) {
+        this.indetifier = indetifier;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", indetifier='" + indetifier + '\'' +
+                ", birth='" + birth + '\'' +
+                '}';
+    }
+}
