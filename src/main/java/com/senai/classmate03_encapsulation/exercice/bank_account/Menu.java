67c529739e6ac75b29d313d898aa7a400698f584
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2025-02-03 03:38:23 -0300

    menu (under construction)

---
+++ src/main/java/com/senai/classmate03_encapsulation/exercice/bank_account/Menu.java
@@ -0,0 +1,30 @@
+package com.senai.classmate03_encapsulation.exercice.bank_account;
+
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void toMenu(){
+        Scanner scan = new Scanner(System.in);
+        int option;
+        String message = """
+                --- Bank System ---
+                1 - deposit
+                2 - withdraw
+                3 - exit
+                """;
+
+        do {
+            System.out.println(message);
+            option = scan.nextInt();
+            switch (option){
+//                case 1 -> ;
+//                case 2 -> ;
+                case 3 -> System.out.println("end");
+                default -> System.out.println("set a valid number");
+
+            }
+        }while (option!=3);
+    }
+}
