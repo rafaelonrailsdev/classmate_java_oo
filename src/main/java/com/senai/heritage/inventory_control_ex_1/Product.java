73eb4bac2d4d76e5af685da92740236ddb84fa28
rafaelonrails <rafaelmoreiralopes41@gmail.com>
2025-02-21 17:21:04 -0300

    new lists

--- src/main/java/com/senai/heritage/inventory_control_ex_1/Product.java
+++ src/main/java/com/senai/heritage/inventory_control_ex_1/Product.java
@@ -1,15 +1,27 @@
 package com.senai.heritage.inventory_control_ex_1;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public abstract class Product {
-    private String name;
+    private  String name;
     private double price;
     private int amount;
-    static ArrayList<Product> products = new ArrayList<>();
+    static ArrayList<Product> listProduct = new ArrayList<>();
+    static ArrayList<Food> listFood = new ArrayList<>();
+    static ArrayList<Eletronic> listEletronic = new ArrayList<>();
+
     public Product(String name, Double price, int amount){
         this.name = name;
         this.price = price;
         this.amount = amount;
     }
+    public static void showStock(){
+            System.out.println(listProduct.toString().replaceAll(",",""));
+    }
+
+    public String getName() {return name;}
+    public double getPrice() {return price;}
+    public int getAmount() {return amount;}
+
 }
