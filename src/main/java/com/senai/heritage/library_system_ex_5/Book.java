f439971d629bfe9bd64810ebd311f74ef207c68c
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2025-03-07 11:13:41 -0300

    feat

--- src/main/java/com/senai/heritage/library_system_ex_5/Book.java
+++ src/main/java/com/senai/heritage/library_system_ex_5/Book.java
@@ -3,4 +3,16 @@ package com.senai.heritage.library_system_ex_5;
 public class Book {
     String title;
     String author;
+
+    // Construtor para inicializar título e autor
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    // Método para exibir informações básicas do livro
+    public void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+    }
 }
--- src/main/java/com/senai/heritage/library_system_ex_5/Ebook.java
+++ src/main/java/com/senai/heritage/library_system_ex_5/Ebook.java
@@ -1,5 +1,18 @@
 package com.senai.heritage.library_system_ex_5;
 
-public class Ebook {
-    String fileSizeInMb;
+class Ebook extends Book {
+    double fileSizeInMb;
+
+    // Construtor para inicializar título, autor e tamanho do arquivo
+    public Ebook(String title, String author, double fileSizeInMb) {
+        super(title, author);
+        this.fileSizeInMb = fileSizeInMb;
+    }
+
+    // Método para exibir informações específicas do Ebook
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("File Size: " + fileSizeInMb + " MB");
+    }
 }
